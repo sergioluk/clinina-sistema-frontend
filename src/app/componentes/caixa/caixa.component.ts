8087c13b1b923b82bb421d81b0229fb13bf478dd
Sergio Rocha <sergiorsj1@hotmail.com>
2024-07-19 22:07:24 -0300

    abrir fechar caixa

--- src/app/componentes/caixa/caixa.component.ts
+++ src/app/componentes/caixa/caixa.component.ts
@@ -12,6 +12,7 @@ export class CaixaComponent {
   end!: Date;
   modoEdicaoAbertura = false;
   modoEdicaoDespesas = false;
+  caixaAberto = false;
 
   constructor(private icone: IconeService) {}
 
